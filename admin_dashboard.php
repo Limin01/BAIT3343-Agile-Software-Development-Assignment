c4e1a4b474b5babe15e68f4bc1a3d0001a3523d6
sje02s <90133675+sje02s@users.noreply.github.com>
2023-04-13 19:59:14 +0800

    Add files via upload

---
+++ admin_dashboard.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Dashboard</title>
+        <link rel="stylesheet" type="text/css" href="css/admin.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="sidebar">
+                <h2>Food Catering System</h2>
+                <ul>
+                    <li><a href="admin_dashboard.php">Dashboard</a></li>
+                    <li><a href="#">Users</a></li>
+                    <li><a href="admin_menu.php">Menu</a></li>
+                    <li><a href="#">Orders</a></li>
+                </ul>
+            </div>
+            <div class="main-content">
+                <h1>Welcome to the Admin Panel</h1>
+            </div>
+        </div>
+    </body>
+</html>
